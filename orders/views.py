d684f724497de9944d80452709742d3e776d2443
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-28 22:48:35 +0530

    Custom admin view page

--- orders/views.py
+++ orders/views.py
@@ -24,3 +24,13 @@ def order_create(request):
         form = OrderForm()
     
     return render(request, "orders/order_create.html", {"form": form})
+
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/order_detail.html", {"order": order})
