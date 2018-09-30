d5f2495184c287f9324f0bfa796a1580f5e52939
ABHISHEK TIWARI <abhie.lp@gmail.com>
2018-09-30 08:29:25 +0530

    Send invoice through pdf

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 import braintree
 from orders.models import Order
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from io import BytesIO
+import weasyprint
 
 def payment_process(request):
     order_id = request.session.get("order_id")
@@ -19,6 +24,16 @@ def payment_process(request):
             order.paid = True
             order.braintree_id = result.transaction.id
             order.save()
+
+            subject = "My Shop - Invoive no. {}".format(order.id)
+            message = "Please, ind attached invoice for your recent purchase."
+            email = EmailMessage(subject, message, "admin@myshop.com", [order.email])
+            html = render_to_string("orders/order_pdf.html", {"order": order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+            email.attach("order_{}.pdf".format(order.id), out.getvalue(), "application/pdf")
+            email.send()
             return redirect("payment:done")
         else:
             return redirect("payment:canceled")
